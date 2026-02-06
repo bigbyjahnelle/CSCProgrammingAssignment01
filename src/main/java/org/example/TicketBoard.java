2c4bfce38c4bc6ff4b02a837febaf7f212de4a1e
jahnellebigby <bigbjl@farmingdale.edu>
2026-02-06 14:55:14 -0500

    I did TODO #5

--- src/main/java/org/example/TicketBoard.java
+++ src/main/java/org/example/TicketBoard.java
@@ -85,6 +85,21 @@ public class TicketBoard {
     // ---------------------------------------------------------
     public Ticket findByIdBinarySearch(int id) {
         // TODO #5
+        int left = 0;
+        int right = size - 1;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            int midId = tickets[mid].getId();
+
+            if (midId == id){
+                return tickets[mid];
+            }else if(midId < id){
+                left = mid +1 ;
+            }else {
+                right = mid - 1;
+            }
+        }
         return null;
     }
 
